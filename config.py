3503b5526586e7fd46c313f955aec8bac5db6625
roninfr1508-rgb <roninfr1508@gmail.com>
2026-05-15 01:31:35 +0500

    Update BOT_TOKEN to use a placeholder
    Replaced the actual bot token with a placeholder.

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "8844610626:AAENfL8FCDRaQ4cYnadRknE4txiL5nSqjgg"
+BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
 WEBHOOK_PATH = f"/{BOT_TOKEN}"
 WEBHOOK_URL = "https://payable-previous-debtor.ngrok-free.dev"
